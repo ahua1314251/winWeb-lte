4923b334830d4b56abfe68bfdd469f09863fa4a9
tom <ahua1314251@163.com>
2019-11-07 23:56:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');//用于自动生成htm
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");//将CSS代码提取为独立文件的插件
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");//CSS模块资源优化插件
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports={
     mode: 'production',
     entry:{
@@ -31,6 +33,7 @@ module.exports={
             }
         ]},
     plugins:[
+        new CleanWebpackPlugin(),
         // new HtmlWebpackPlugin(),//生成入口html文件
         new MiniCssExtractPlugin({
             filename: "[name].css"
@@ -41,32 +44,54 @@ module.exports={
             template:"./starter.html",
             filename:"starter.html"
 
+        }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                sourceMap: false,
+                compress: {
+                    // 删除所有的 `console` 语句，可以兼容ie浏览器
+                    drop_console: true,
+                    // 内嵌定义了但是只用到一次的变量
+                    collapse_vars: true,
+                    // 提取出出现多次但是没有定义成变量去引用的静态值
+                    reduce_vars: true
+
+                },
+                output: {
+                    // 最紧凑的输出
+                    beautify: false,
+                    // 删除所有的注释
+                    comments: false
+                }
+            }
         })
     ],
     devServer: {
-        contentBase:__dirname + '/dist',
+        contentBase:__dirname + '/dists',
         compress: true,
         port: 9000
     },
     optimization: {
         splitChunks: {
             chunks: "all",//在做代码分割时，只对异步代码生效，写成all的话，同步异步代码都会分割
-            minSize: 500000, //引入的包大于500KB才做代码分割
+            minSize: 300000, //引入的包大于500KB才做代码分割
+            maxSize: 400000,
             minChunks: 1, //当一个包至少被用了多少次的时候才进行代码分割
-            maxAsyncRequests: 5, //同时加载的模块数最多是5个
-            maxInitialRequests: 3, //入口文件做代码分割最多能分成3个js文件
+            maxAsyncRequests: 8, //同时加载的模块数最多是5个
+            maxInitialRequests: 6, //入口文件做代码分割最多能分成3个js文件
             automaticNameDelimiter: '~',//文件生成时的连接符
             name: true,//让cacheGroups里设置的名字有效
             cacheGroups: {//当打包同步代码时,上面的参数生效
                 vendors: {
-                    test: /[\\/]node_modules[\\/]/, //检测引入的库是否在node_modlues目录下的
                     priority: -10,//值越大,优先级越高.模块先打包到优先级高的组里
                     filename: 'vendors.js'//把所有的库都打包到一个叫vendors.js的文件里
                 },
-                default: {
+                commom: {
                     priority: -20,
                     reuseExistingChunk: true,//如果一个模块已经被打包过了,那么再打包时就忽略这个上模块
                     filename: 'common.js'
+                    // test: /[\\/]node_modules[\\/]/ //检测引入的库是否在node_modlues目录下的
+                    // minChunks: 2
                 }
             }
         }
